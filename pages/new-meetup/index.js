a41f264e40555abcaedaf810bbd1c9a118acdf3f
unknown <a-f-r@hotmail.es>
2021-07-28 23:44:57 +0200

    Add the New Meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
-  return <div></div>;
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetup;
