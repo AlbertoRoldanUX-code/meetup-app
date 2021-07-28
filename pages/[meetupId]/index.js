517e4ae2b46ebd0deb729cd77ff9f5f39b4b2eb3
unknown <a-f-r@hotmail.es>
2021-07-29 01:57:57 +0200

    Create MeetupDetail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function meetupId() {
-  return <div></div>;
+  return (
+    <MeetupDetail
+      image='https://cdn2.civitatis.com/suiza/zurich/guia/zurich-card.jpg'
+      title='First Meetup'
+      address='Flüelastrasse 5, 12345 Zürich'
+      description='This is a 1º meetup'
+    />
+  );
 }
 
 export default meetupId;
