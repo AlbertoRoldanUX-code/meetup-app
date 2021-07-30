628877881723547ddf85abdbdfeafb1d29237a79
unknown <a-f-r@hotmail.es>
2021-07-30 02:18:26 +0200

    Make API route to insert meetups into a mongodb database.

--- pages/api/new-meetups.js
+++ pages/api/new-meetups.js
@@ -1,9 +1,25 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+const handler = async function (req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, decription } = data;
+    const client = await MongoClient.connect(
+      process.env.REACT_APP_MONGODB_API_KEY
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted' });
   }
-}
+};
 
 export default handler;
+
+//
