8fde841b1e868f91284764db4305f2c6408b3f83
unknown <a-f-r@hotmail.es>
2021-08-01 20:10:48 +0200

    Send http request to new-meetups route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
-  function addMeetupHandler(meetupData) {
-    console.log(meetupData);
-  }
+  const router = useRouter();
+  const addMeetupHandler = async function (meetupData) {
+    try {
+      const res = await fetch('/api/new-meetups', {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await res.json();
+      console.log(data);
+      router.push('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
